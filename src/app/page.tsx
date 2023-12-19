ca37151798d09ccdf0f4543a9f1ded69ecdeb5ae
beauty <beauty@skuniv.ac.kr>
2023-12-20 01:55:45 +0900

    # 2주차: Next.js AppRouter --- - 첫화면 = 로그인 페이지 - 서경대학교 로고는 전체 페이지에 유지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-      <main>Hello, Nextjs!</main>
+      <main>
+          <h2>서경대학교 전화번호부 관리 Backoffice</h2>
+          <h3>로그인 페이지</h3>
+      </main>
   )
 }
