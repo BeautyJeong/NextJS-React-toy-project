7ce66d2f4a90f8b27d354ec3a5e465af8f74fdb8
beauty <wjdqbxl@gachon.ac.kr>
2023-12-26 20:36:53 +0900

    dashboard 디렉토리 재구성

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html>
+        <body>
+        {children}
+        </body>
+        </html>
+    )
+}
